e4a754547c5f3cde7e2fb5159744ea919078b81f
perseusdevelopment <68746756+perseusdevelopment@users.noreply.github.com>
2020-09-01 11:13:04 -0400

    Add files via upload

---
+++ jsstalker.js
@@ -0,0 +1,10 @@
+var firstName = prompt("What is your First Name?")
+var lastName = prompt("What is your Last Name?")
+var age = prompt("What is your Age?")
+
+var fullName = firstName + " " + lastName;
+
+console.log("Your FULL Name is " + fullName);
+console.log("You are " + age + " " + "years old!");
+
+
