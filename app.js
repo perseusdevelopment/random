3ba3c5f0a3b3324fef546cea54c16b7960dbd5c6
Brett Warnebold <68746756+perseusdevelopment@users.noreply.github.com>
2020-09-03 14:45:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var app = express();
 
-app.get ("/", function(req, res){
+app.get("/", function (req, res) {
     res.send("Hi There!");
+});
+
+app.get("/r/:subRedditName", function (req, res) {
+    var subReddit = req.params.subRedditName;
+    res.send("Welcome to the " + subReddit.toUpperCase() + " Subreddit");
+});
+
+app.get("/r/:subRedditName/comments/:id/:title/", function (req, res) {
+    res.send("Welcome to a Deep! Subpage");
 });
 
 app.get("/bye", function (req, res) {
@@ -10,12 +19,12 @@ app.get("/bye", function (req, res) {
 });
 
 app.get("/dog", function(req, res){
-    res.send("Meow!")
+    res.send("Meow!");
 });
 
-app.get("*", function (req, res) {
+/* app.get("*", function (req, res) {
     res.send("You Are A Star!");
-});
+}); */
 
 app.listen(3000, function(){
     console.log("Server Has Started");
